eb4fca7fb2654961e10e950d1e5b038ad115427c
Jcobian <jcobianits@gmail.com>
2020-11-12 14:08:19 -0600

    Solo menu principal

---
+++ app/src/main/java/com/tecmm/ejemplomenu/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.tecmm.ejemplomenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean{
+        val inflate: MenuInflater = menuInflater
+        inflate.inflate(R.menu.menu_principal,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean
+            = when(item.itemId){
+        R.id.menus_configuracion -> {
+            Log.e("MENU","Se presiono configuracion")
+            true
+        }
+        R.id.menus_acercade->{
+            Log.e("MENU","Se presiono Acerca de")
+            true
+        }
+        else -> {
+            Log.e("MENU","Se presiono usuarios")
+            true
+        }
+    }
+
+}
